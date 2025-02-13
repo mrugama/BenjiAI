5b8f9fa0292ab100c4388f9b9c2e7d821081a25d
Marlon Rugama <mrugama@gmail.com>
2025-02-13 11:22:13 -0500

    Initial Commit

---
+++ Clipper/ClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipperApp.swift
+//  Clipper
+//
+//  Created by Marlon Rugama on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
