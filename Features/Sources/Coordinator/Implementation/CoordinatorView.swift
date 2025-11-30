9c7c62c50be5f8c71145d92fce1e2eb3b14e80cf
Marlon Rugama <mrugama@gmail.com>
2025-11-30 17:40:33 -0500

    Added swiftlint to prevent public keyword in the implementation folder

--- Features/Sources/Coordinator/Implementation/CoordinatorView.swift
+++ Features/Sources/Coordinator/Implementation/CoordinatorView.swift
@@ -23,9 +23,3 @@ struct CoordinatorView: View {
         }
     }
 }
-
-public struct MyView: View {
-    public var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
-    }
-}
